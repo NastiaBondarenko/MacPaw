1508d876db8e1ceb28c662e0fd35d53a910aad0f
nastiabondarenko17@gmail.com <nastiabondarenko17@gmail.com>
2022-07-24 22:20:33 +0300

    code refactoring

--- test-app/tests/Feature/NeoTest.php
+++ test-app/tests/Feature/NeoTest.php
@@ -31,16 +31,59 @@ class NeoTest extends TestCase
 
         $response
             ->assertStatus(200);
-        
+
+        $asteroids = json_decode($response->getBody()); 
+        foreach($asteroids as $asteroid){
+            assert($asteroid->{'ishazardous'} == '1');
+        }
     }
 
-    public function test_a_fastest_request()
+    public function test_a_fastest_request_without_hazardous_parametr()
     {
         $response = $this->get('/neo/fastest');
 
         $response
             ->assertStatus(200);
-        
+        $asteroids = json_decode($response->getBody()); 
+        foreach($asteroids as $asteroid){
+            assert($asteroid->{'ishazardous'} == '0');
+        }   
+    }
+
+    public function test_a_fastest_request_with_hazardous_true()
+    {
+        $response = $this->get('/neo/fastest?hazardous=true');
+
+        $response
+            ->assertStatus(200);
+        $asteroids = json_decode($response->getBody()); 
+        foreach($asteroids as $asteroid){
+            assert($asteroid->{'ishazardous'} == '1');
+        }   
+    }
+
+    public function test_a_fastest_request_with_hazardous_false()
+    {
+        $response = $this->get('/neo/fastest?hazardous=false');
+
+        $response
+            ->assertStatus(200);
+        $asteroids = json_decode($response->getBody()); 
+        foreach($asteroids as $asteroid){
+            assert($asteroid->{'ishazardous'} == '0');
+        }   
+    }
+
+    public function test_a_fastest_request_with_wrong_hazardous()
+    {
+        $response = $this->get('/neo/fastest?hazardous=err');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                    'code'      =>  400,
+                    'message'   =>  'The hazardous parameter must be true or false'
+            ]);
     }
 
 }
